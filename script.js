a57e5aa854f05fed6a1ed0dbbfdcfa41e1f41740
Shohjahon <shohjahonorinboyev896@gmail.com>
2023-05-28 11:17:06 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// const person  = {
+//   firstName:"O'rinboyev",
+//   lastName: "Shohjahon",
+//   age:"19"
+// }
+// console.log(person);
+
+// confirm("Siz 18 yoshdamisiz");
+// const userName = prompt("Ismingiz nima?");
+// console.log(userName);
+
+// const firstName = "Shohjahon";
+// const age = "20";
+// console.log(`Mani ismim ${firstName}`);
+
+// const xName = prompt("Enter your name", "");
+// console.log(`User's name ${xName}`);
+
+// const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+
+// const personalNewsDB = {
+//   count: numberOfNews,
+//   news: {},s
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// const a = prompt("Oxirgi ko'rgan yangiliklaringizdan biri"),
+//   b = +prompt("Unga qancha baxo bergan bo'lar edingiz");
+
+// personalNewsDB.news[a] = b;
+// console.log(personalNewsDB);
